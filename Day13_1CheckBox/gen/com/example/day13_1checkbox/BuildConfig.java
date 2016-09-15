471bcd2d8c39260a30472523fe3c58898aadff28
SwimingKim <rumex13@naver.com>
2016-09-15 09:32:40 +0900

    Add files via upload

---
+++ Day13_1CheckBox/gen/com/example/day13_1checkbox/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.day13_1checkbox;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Day13_2CheckBox2/gen/com/example/day13_2checkbox2/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.day13_2checkbox2;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
