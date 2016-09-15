dad966a099c56dc1a471398e0fdd8930fb591d09
SwimingKim <rumex13@naver.com>
2016-09-15 09:26:30 +0900

    Add files via upload

---
+++ Day11_2TextView2/gen/com/example/day11_2textview2/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.day11_2textview2;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Day12_0HelloWorld/gen/com/example/day12_0helloworld/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.day12_0helloworld;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
