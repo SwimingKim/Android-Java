248c3dd1acb06292999edc455d586fb46dca10f4
김수영 <rumex13@naver.com>
2016-12-30 03:11:12 -0800

    맥북 커밋 테스트

---
+++ Project03_Sea/src/com/su/project03/ChoiceActivity.java
@@ -0,0 +1,12 @@
+package com.su.project03;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChoiceActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+	}//end of onCreate
+}//end of class
